feffb3a026c5ca138075e354370ff9cda55e2d24
thanh <miktenthanh@gmail.com>
2022-10-14 02:16:47 +0700

    complete

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ icon_menu.addEventListener("click", function () {
   menu.classList.toggle("invisible");
   console.log(menu);
 });
+
+var bnt = document.querySelector(".hehe");
+
+bnt.addEventListener("click", function () {
+  alert("HET THOI GIAN DANG KY");
+});
+var bnt2 = document.querySelector(".hehe2");
+
+bnt2.addEventListener("click", function () {
+  alert("HET THOI GIAN DANG KY");
+});
+var bnt3 = document.querySelector(".hehe3");
+
+bnt3.addEventListener("click", function () {
+  alert("HET THOI GIAN DANG KY");
+});

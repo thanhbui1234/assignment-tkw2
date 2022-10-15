1192a777103d0f005d66465bd6782e97e44e2904
thanh <miktenthanh@gmail.com>
2022-10-16 05:34:53 +0700

    finish

--- test.js
+++ test.js
@@ -12,3 +12,31 @@ var end3 = document.querySelector("#end3");
 end3.addEventListener("click", function () {
   alert("Da het thoi gian"); // TODO
 });
+var test_1 = document.querySelector("#btn_1");
+test_1.addEventListener("click", function () {
+  alert("Ban da dang ky"); // TODO
+});
+var test_2 = document.querySelector("#btn_2");
+test_2.addEventListener("click", function () {
+  alert("Ban da dang ky"); // TODO
+});
+
+var test_3 = document.querySelector("#btn_3");
+test_3.addEventListener("click", function () {
+  alert("Ban da dang ky"); // TODO
+});
+
+var test_4 = document.querySelector("#btn_4");
+test_4.addEventListener("click", function () {
+  alert("Ban da dang ky"); // TODO
+});
+
+var test_5 = document.querySelector("#btn_5");
+test_5.addEventListener("click", function () {
+  alert("Ban da dang ky"); // TODO
+});
+
+var test_6 = document.querySelector("#btn_6");
+test_6.addEventListener("click", function () {
+  alert("Ban da dang ky"); // TODO
+});
